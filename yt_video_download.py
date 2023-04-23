ed3f567b8cee8292d86f2aba7a0ec7d9844f7daf
Joao Matias <joaopedrord2001@gmail.com>
2023-04-23 19:14:46 -0300

    youtube video download

---
+++ yt_video_download.py
@@ -0,0 +1,12 @@
+from pytube import YouTube
+
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print('An error has occured')
+    
+link = input('Video URL: ')
+Download(link)
